926a0281a13f094b1177e3fda41c268cabed73fe
Demys <demysdcl@gmail.com>
2019-11-12 00:28:41 -0300

    initial configuration

---
+++ src/routes/index.svelte
@@ -0,0 +1,57 @@
+<script>
+  const title = process.env.TITLE
+</script>
+
+<style lang="stylus">
+  @import '../../static/variables';
+
+  h1
+  h5
+  figure
+  p
+    text-align: center
+    margin: 0 auto
+    color: $stylus-color
+    
+  h1
+    font-size: 2.8em
+    font-weight: 700
+    margin: 0 0 0.5em 0
+    text-transform: uppercase
+    
+
+  figure
+    margin: 0 0 1em 0
+  
+    img
+      width: 100%
+      max-width: 400px
+      margin: 0 0 1em 0
+
+  p
+    margin: 1em auto
+
+
+  @media (min-width: 480px)
+    h1
+      font-size: 4em
+
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h5>{title}</h5>
+<h1>Great success!</h1>
+
+<figure>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
